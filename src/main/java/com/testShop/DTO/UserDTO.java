1b397eacb303b64ef9dd0809c0b358afdae7a324
igr76 <gritsuk76@mail.ru>
2023-05-11 21:53:31 +1000

    process

---
+++ src/main/java/com/testShop/DTO/UserDTO.java
@@ -0,0 +1,55 @@
+package com.testShop.DTO;
+
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+/**
+ * DTO сущности
+ */
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class UserDTO {
+
+    /**
+     * почта пользователя
+     */
+    String email;
+
+    /**
+     * Имя пользователя
+     */
+    String firstName;
+    /**
+     * id пользователя
+     */
+    Integer id;
+    /**
+     * Фамилия пользователя
+     */
+    String lastName;
+    /**
+     * телефон пользователя
+     */
+    String phone;
+    /**
+     * дата регистрации пользователя
+     */
+    String regDate;
+    /**
+     * город пользователя
+     */
+    String city;
+    /**
+     * фото пользователя
+     */
+
+    String image;
+
+}
+
