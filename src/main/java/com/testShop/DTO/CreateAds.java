f082874011ca4df424201d5cbdeb8c77a6b7e37e
igr76 <gritsuk766@gmail.com>
2023-07-06 07:07:06 +1000

    process

--- src/main/java/com/testShop/DTO/CreateAds.java
+++ src/main/java/com/testShop/DTO/CreateAds.java
@@ -1,4 +1,35 @@
 package com.testShop.DTO;
 
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import javax.validation.constraints.Min;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class CreateAds {
+
+    /**
+     * Описание продаваемого объекта
+     */
+    String description;
+
+    /**
+     * цена
+     */
+    @Min(1)
+    Integer price;
+
+    /**
+     * Название
+     */
+    String title;
+
+
 }
